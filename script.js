8eb0f5be9a7be55ff3bb61a5094a83d0f76c148e
Jaya242 <jayaarora242@gmail.com>
2025-02-03 17:17:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
+var a=document.querySelector("#elem1")
+var abc=console.log(a.getAttribute("class"))
